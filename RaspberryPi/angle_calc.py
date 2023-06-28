b61f2bee5e304a5fc5cee04083bb44d581165900
jy3119 <jy3119@ic.ac.uk>
2023-06-28 08:13:47 +0100

    added

--- RaspberryPi/angle_calc.py
+++ RaspberryPi/angle_calc.py
@@ -1,39 +1,20 @@
-import serial
-import time
-
-# Connect to the Arduino
-ser = serial.Serial('/dev/ttyACM0', 9600)
-time.sleep(2) # wait for the serial connection to initialize
-
-def calculate_angle():
-    min_obstacle_angle = None
-    min_obstacle_value = float('inf')
-
+def main():
+    ser = serial.Serial('/dev/ttyACM0', 9600)
+    data = {}
     while True:
-        if ser.in_waiting:
-            # Read a line from the serial port
-            line = ser.readline().decode('utf-8').strip()
-            # Parse the data
-            data = line.split(',')
-            angle = int(data[0])
-            top = int(data[1]) if data[1] != '-1' else float('inf')
-            middle = int(data[2]) if data[2] != '-1' else float('inf')
-            bottom = int(data[3]) if data[3] != '-1' else float('inf')
+        line = ser.readline()
+        parsed_data = parse_data(line)
+        if parsed_data is None:
+            continue
+        angle, top, middle, bottom, _ = parsed_data
+        if angle not in data:
+            data[angle] = []
+        data[angle].append(top)
+        data[angle].append(middle)
+        data[angle].append(bottom)
 
-            # Find the maximum obstacle value for this angle
-            max_obstacle_value = max(top, middle, bottom)
-
-            # If this angle has less obstacles, update our min_obstacle variables
-            if max_obstacle_value < min_obstacle_value:
-                min_obstacle_value = max_obstacle_value
-                min_obstacle_angle = angle
-
-        # If we have finished a scan (back at 0 degrees), break the loop
-        if angle == 0 and min_obstacle_angle is not None:
-            break
-
-    return (min_obstacle_angle*-1)  + 90
-
-# Run the function to get the angle with the least obstacles
-angle = calculate_angle()
-print(f"The angle with the least obstacles is: {angle}")
+        # Process the data once we have collected from all angles
+        if len(data) == 19:  # 19 distinct angles from 0 to 180 in steps of 10
+            best_angle = find_best_angle(data)
+            print("Best angle: ", best_angle)
+            data = {}  # reset the data
